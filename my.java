066cb4be23954bbad3ace61270863bfc2b68cbc4
pepyaka <pepya.ka@mail.ru>
2025-04-28 00:58:29 +0700

    Изменение кода

--- my.java
+++ my.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, мир!");
-        System.out.println("Hello from the other side!");
+        System.out.println("Это новая фича!");
     }
 }
